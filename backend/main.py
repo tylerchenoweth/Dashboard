ad3bc064b9f40a5b4aa319f9e12e1b9746054ec0
sh4ck1et0n <tchen358@gmail.com>
2025-02-15 16:13:22 -0500

    added cors and middleware to help the frontend connect

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from routers import pokeapi_router
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+# Allow requests from your frontend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Change "*" to your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def first_example():
